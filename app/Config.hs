0a612107b2d142efa98e603e7b46710ee9b1a161
dimasinger <41106023+dimasinger@users.noreply.github.com>
2019-06-27 15:00:01 +0200

    initial setup

---
+++ app/Config.hs
@@ -0,0 +1,8 @@
+module Config () where
+
+import Data.ConfigFile
+
+configFilePath = "config/autobank.ini"
+
+sheetsSection = "sheets"
+bankingSection = "banking"
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Config
 
 main :: IO ()
-main = someFunc
+main = putStrLn "???"
